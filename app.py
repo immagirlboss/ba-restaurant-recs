8f909bcabdba0e9232b5cbbf9b0167a4497dfb19
Akbota Kengeskhan <akbota@uni.minerva.edu>
2026-04-16 20:04:08 -0300

    Add Docker deploy support

--- app.py
+++ app.py
@@ -64,5 +64,8 @@ def reset():
         sessions.pop(session["session_id"], None)
     return jsonify({"status": "success"})
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
